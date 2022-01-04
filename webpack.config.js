73987a46d1952571833ca0b550f50e6df2ca04ad
goo-gy <lkukh17@naver.com>
2022-01-05 01:35:29 +0900

    webpack setting
    * entry
    * output
    * loader (babel)
    
    #1

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  },
+};
