f13e9fdb6d62f4567dbc4cf504858425e15fd86f
goo-gy <lkukh17@naver.com>
2022-01-08 00:57:11 +0900

    TailWind set-up
    - install tailwind
    - add style, css, postcss loader
    - add tailwind config
    
    #1, #2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const App = (props) => {
   return (
-    <div>
-      <h1>Prettier Test</h1>
-      Webpack App! Auto Save?!
+    <div className="bg-red-300">
+      <h1 className="text-3xl font-bold underline text-center">
+        TailWind Test
+      </h1>
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
