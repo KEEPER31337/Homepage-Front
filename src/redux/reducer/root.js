bcf3bc208e64e9fb0ffdfde206ddc96fa1871a4e
goo-gy <lkukh17@naver.com>
2022-02-08 10:53:02 +0900

    feature) redux-persist
    - redux-persist를 이용해 redux state를 local storage에 저장
    - 현재는 필터링 없이 모든 redux state를 local storage에 저장하고 있음
    
    #4, #23

--- src/redux/reducer/root.js
+++ src/redux/reducer/root.js
@@ -1,10 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 // local
 import reducerDarkMode from './darkMode';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  // TODO : whitelist
+};
 
 const rootReducer = combineReducers({
   darkMode: reducerDarkMode,
 });
 
-export default rootReducer;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default persistedReducer;
