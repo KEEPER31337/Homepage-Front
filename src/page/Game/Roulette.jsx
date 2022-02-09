7f0fe4cc27d3c7206da98b106992bf801d3bddeb
kej <publdaze@naver.com>
2022-02-09 12:55:10 +0900

    Game(Roulette) 돌리기
    - Roulette spin animation 적용
    - Roulette spin 실행 시간 적용
    
    #20

--- src/page/Game/Roulette.jsx
+++ src/page/Game/Roulette.jsx
@@ -2,39 +2,69 @@ import React from 'react';
 import { useState } from 'react';
 
 const Roulette = () => {
+  const [ani, setAni] = useState('animate-none');
+  const onClick = () => {
+    console.log('click');
+    setAni('animate-spin');
+    setTimeout(function () {
+      setAni('animate-none');
+    }, 3000);
+  };
+
   return (
     <div className="border border-divisionGray items-center m-10">
       <div className="m-0 p-0 bg-slate-600 flex items-center justify-center overflow-hidden">
-        <div className="w-52 h-52 bg-slate-100 border-solid border-8 rounded-full relative overflow-hidden duration-500">
+        <div
+          className={`${ani} w-52 h-52 bg-slate-100 border-solid border-8 rounded-full relative overflow-hidden`}
+        >
           <div className="absolute left-1/2 -translate-x-1/2 z-20 flex items-center justify-center">
             <div className="w-0 h-0 border-t-green-600 border-t-[15px] border-r-transparent border-r-[5px] border-l-transparent border-l-[5px]"></div>
           </div>
 
-          <button className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 bg-cyan-300 uppercase border-8 border-solid border-cyan-600 font-bold text-gray-500 w-20 h-20 rounded-full cursor-pointer outline-none">
+          <button
+            onClick={onClick}
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 bg-cyan-300 uppercase border-8 border-solid border-cyan-600 font-bold text-gray-500 w-20 h-20 rounded-full cursor-pointer outline-none"
+          >
             Spin
           </button>
-          <div className="[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-red-600 h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-1/2">
+          <div
+            className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-red-600 h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-1/2`}
+          >
             1
           </div>
-          <div className="[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-orange-600 rotate-[45deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24">
+          <div
+            className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-orange-600 rotate-[45deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+          >
             2
           </div>
-          <div className="[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-yellow-600 rotate-[90deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24">
+          <div
+            className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-yellow-600 rotate-[90deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+          >
             3
           </div>
-          <div className="[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-green-600 rotate-[135deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24">
+          <div
+            className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-green-600 rotate-[135deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+          >
             4
           </div>
-          <div className="[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-blue-600 rotate-[180deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24">
+          <div
+            className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-blue-600 rotate-[180deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+          >
             5
           </div>
-          <div className="[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-purple-600 rotate-[225deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24">
+          <div
+            className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-purple-600 rotate-[225deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+          >
             6
           </div>
-          <div className="[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-gray-600 rotate-[270deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24">
+          <div
+            className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-gray-600 rotate-[270deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+          >
             7
           </div>
-          <div className="[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-slate-100 rotate-[315deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24">
+          <div
+            className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-slate-100 rotate-[315deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+          >
             8
           </div>
         </div>
