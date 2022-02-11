fe321855e0d5a994cbae940b971fa5245d54992f
kej <publdaze@naver.com>
2022-02-11 11:21:32 +0900

    Game(Roulette) 외관 수정
    - 포인트 들어가는 polygon(삼각형) 크기 조정
    - 색 조정
    - shadow 적용
    - rotate 적용 안 되는 문제 해결..중
    
    #20

--- src/page/Game/Roulette.jsx
+++ src/page/Game/Roulette.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 const Roulette = () => {
   const points = [1, 2, 3, 4, 5, 6, 7, 8];
-  const pointIdx = 7;
+  const pointIdx = 4;
   const [stopPoint, setStopPoint] = useState(0);
   const [ani, setAni] = useState('animate-none');
   const onClick = () => {
@@ -11,80 +11,70 @@ const Roulette = () => {
     setAni('animate-spin');
     setStopPoint(points.length - pointIdx);
     setTimeout(function () {
-      setAni('animate-none');
+      setAni(
+        'animate-none' +
+          ' rotate-[' +
+          ((0 + 45 * (points.length - pointIdx)) % 360) +
+          'deg]'
+        // stopPoint setStopPoint 적용한 값 안 돼서 일단 계산식으로 바로 박아 놓음
+      );
     }, 3000);
   };
 
   return (
     <div className="border border-divisionGray items-center m-10">
-      <div className="m-0 p-0 bg-slate-600 flex items-center justify-center overflow-hidden">
+      <div className="m-0 p-0 bg-white flex items-center justify-center overflow-hidden">
         <div className="relative">
           <div
-            className={`${ani} w-52 h-52 bg-slate-100 border-solid border-8 rounded-full relative overflow-hidden`}
+            className={`shadow-md shadow-gray-700 ${ani} w-52 h-52 bg-slate-100 border-black border-solid border-8 rounded-full relative overflow-hidden`}
           >
             <div
-              className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-red-600  rotate-[${
-                (0 + 45 * stopPoint) % 360
-              }deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-1/2`}
+              className={`[clip-path:polygon(92%_0,_50%_100%,_8%_0)] bg-[#940202f3] rotate-[0deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-1/2`}
             >
               {points[0]}
             </div>
             <div
-              className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-orange-600 rotate-[${
-                (45 + 45 * stopPoint) % 360
-              }deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+              className={`[clip-path:polygon(92%_0,_50%_100%,_8%_0)] bg-[#780103] rotate-[45deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
             >
               {points[1]}
             </div>
             <div
-              className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-yellow-600 rotate-[${
-                (90 + 45 * stopPoint) % 360
-              }deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+              className={`[clip-path:polygon(92%_0,_50%_100%,_8%_0)] bg-[#5a0606] rotate-[90deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
             >
               {points[2]}
             </div>
             <div
-              className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-green-600 rotate-[${
-                (135 + 45 * stopPoint) % 360
-              }deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+              className={`[clip-path:polygon(92%_0,_50%_100%,_8%_0)] bg-[#500305] rotate-[135deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
             >
               {points[3]}
             </div>
             <div
-              className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-blue-600 rotate-[${
-                (180 + 45 * stopPoint) % 360
-              }deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+              className={`[clip-path:polygon(92%_0,_50%_100%,_8%_0)] bg-[#940202f3] rotate-[180deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
             >
               {points[4]}
             </div>
             <div
-              className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-purple-600 rotate-[${
-                (225 + 45 * stopPoint) % 360
-              }deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+              className={`[clip-path:polygon(92%_0,_50%_100%,_8%_0)] bg-[#780103] rotate-[225deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
             >
               {points[5]}
             </div>
             <div
-              className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-gray-600 rotate-[${
-                (270 + 45 * stopPoint) % 360
-              }deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+              className={`[clip-path:polygon(92%_0,_50%_100%,_8%_0)] bg-[#5a0606] rotate-[270deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
             >
               {points[6]}
             </div>
             <div
-              className={`[clip-path:polygon(100%_0,_50%_100%,_0_0)] bg-slate-100 rotate-[${
-                (315 + 45 * stopPoint) % 360
-              }deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
+              className={`[clip-path:polygon(92%_0,_50%_100%,_8%_0)] bg-[#500305] rotate-[315deg] h-1/2 w-1/2 absolute -translate-x-1/2 origin-bottom text-center flex items-center justify-center text-xl font-bold left-24`}
             >
               {points[7]}
             </div>
           </div>
           <div className="absolute top-0 left-1/2 -translate-x-1/2 flex items-center justify-center">
-            <div className="mt-2 w-0 h-0 border-t-green-600 border-t-[15px] border-r-transparent border-r-[5px] border-l-transparent border-l-[5px]"></div>
+            <div className="mt-2 w-0 h-0 border-t-mainYellow border-t-[15px] border-r-transparent border-r-[5px] border-l-transparent border-l-[5px]"></div>
           </div>
           <button
             onClick={onClick}
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-cyan-300 uppercase border-8 border-solid border-cyan-600 font-bold text-gray-500 w-20 h-20 rounded-full cursor-pointer outline-none"
+            className="shadow-xl shadow-[#5c0806] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black uppercase border-[5px] border-solid border-mainYellow font-bold text-gray-500 w-12 h-12 rounded-full cursor-pointer outline-none"
           >
             Spin
           </button>
