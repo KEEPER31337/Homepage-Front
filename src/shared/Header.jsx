319ade94d17af702e36b23628068bc8f84976a23
goo-gy <lkukh17@naver.com>
2022-02-12 12:13:38 +0900

    fix) mobile 헤더 가려지는 버그
    - z-index 높여줌
    
    #33

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -236,7 +236,7 @@ function classNames(...classes) {
 export default function Header() {
   return (
     <>
-      <Popover className="relative bg-mainWhite dark:bg-mainBlack">
+      <Popover className="relative bg-mainWhite dark:bg-mainBlack z-30">
         <div className="max-w-7xl mx-auto px-4 sm:px-6">
           <div className="flex justify-between items-center border-b-2 border-gray-100 dark:border-darkComponent py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
@@ -343,7 +343,7 @@ export default function Header() {
           </Popover.Panel>
         </Transition>
       </Popover>
-      <div className="absolute right-1 z-40">
+      <div className="absolute right-1 z-20">
         <DarkModeSwitch />
       </div>
     </>
