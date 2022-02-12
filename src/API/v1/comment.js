43d3724a26903d56e55a24455eec6e5d80a74f43
정핸모 <gusah009@naver.com>
2022-02-12 13:31:30 +0900

    feat: 게시판, 댓글 API 제작

---
+++ src/API/v1/comment.js
@@ -0,0 +1,117 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+async function create({ boardId, content, ipAddress, parentId, token }) {
+  const options = {
+    method: 'POST',
+    url: API_URL + '/v1/comment/' + boardId,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: {
+      content,
+      ipAddress,
+      parentId,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+async function get({ boardId, page, size }) {
+  const options = {
+    method: 'GET',
+    url: API_URL + '/v1/comment/' + boardId,
+    params: {
+      page,
+      size,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+async function remove({ commentId, token }) {
+  const options = {
+    method: 'GET',
+    url: API_URL + '/v1/comment/' + commentId,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+async function modify({ commentId, content, token }) {
+  const options = {
+    method: 'PUT',
+    url: API_URL + '/v1/comment/' + commentId,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: { content },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+async function like({ commentId, token }) {
+  const options = {
+    method: 'GET',
+    url: API_URL + '/v1/comment/like',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: { commentId },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+async function dislike({ commentId, token }) {
+  const options = {
+    method: 'GET',
+    url: API_URL + '/v1/comment/dislike',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: { commentId },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+export default {
+  create,
+  get,
+  remove,
+  modify,
+  like,
+  dislike,
+};
