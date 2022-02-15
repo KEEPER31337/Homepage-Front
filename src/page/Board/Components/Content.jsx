03dd5734ed3b279ba0702ed2b5a055d9e22fb749
amaranth <81465068+amaran-th@users.noreply.github.com>
2022-02-16 07:47:18 +0900

    [ 댓글 UI 구현 및 게시글 추천/비추천 버튼 UI 개선 ]
    - 댓글 UI 구현
    - 댓글 삭제(자기 댓글인 경우) 버튼
    - 추천/비추천 버튼 눌린 상태 UI 구현
    - 추천/비추천 버튼 기능 api 연동 시도하다가 말았음
    
    #12

--- src/page/Board/Components/Content.jsx
+++ src/page/Board/Components/Content.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { testText } from 'page/Board/testText';
 import '@toast-ui/editor/dist/toastui-editor.css'; //마크다운 편집기 뷰어 에디터
 import { Viewer } from '@toast-ui/react-editor';
@@ -11,15 +11,39 @@ import {
 
 //local
 import { getDateWithFormat } from '../BoardUtil';
+import postAPI from 'API/v1/post';
 
 const Content = ({ state, board }) => {
   //board는 게시글 정보가 담긴 객체
   //console.log(state.member.memberId); //(내 아이디)나중에 업데이트 될거임
   const isDark = state.darkMode; //Dark모드 여부
   const myId = state.member.userInfo.id; //게시글을 보고 있는 나의 정보
+  const [isDisliked, setIsDisliked] = useState(false); //싫어요 여부
+  const [isLiked, setIsLiked] = useState(false); //좋아요 여부
+  const postingId = board.id;
+  const token = state.member.token;
   console.log(board);
   const viwerRef = useRef();
+  const clickLikeHandler = () => {
+    setIsLiked(!isLiked);
+    console.log(isLiked);
+  };
+  const clickDislikeHandler = () => {
+    setIsDisliked(!isDisliked);
 
+    console.log(isDisliked);
+    if (!isDisliked) {
+      postAPI
+        .dislike({
+          type: 'INC',
+          postingId: postingId,
+          token: token,
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    }
+  };
   useEffect(() => {
     const viwerInstance = viwerRef.current.getInstance();
     viwerInstance.setMarkdown(board.content);
@@ -55,13 +79,13 @@ const Content = ({ state, board }) => {
         </p>
       </div>
       {board.writerId == myId ? (
-        <div className=" px-3 absolute w-4/5 flex flex-row-reverse ">
+        <div className="px-10 absolute w-full text-xs sm:w-4/5 sm:text-base flex flex-row-reverse ">
           <div className=" rounded-lg bg-slate-100 my-2">
             <UserCircleIcon className="inline-block h-5 w-5 m-1 text-divisionGray " />
-            <button className="text-mainYellow mx-1 hover:text-pointYellow">
+            <button className="text-mainYellow font-bold mx-1 hover:text-pointYellow">
               글 수정
             </button>
-            <button className="text-red-400 mx-2 hover:text-red-600 ">
+            <button className="text-red-400 font-bold mx-2 hover:text-red-600 ">
               <span className="">글 삭제</span>
             </button>
           </div>
@@ -80,12 +104,44 @@ const Content = ({ state, board }) => {
       </div>
 
       <div className="border-y-2 text-center bg-backGray shadow-lg dark:bg-darkPoint dark:border-darkComponent">
-        <button className=" border-4 m-2 border-white rounded-xl shadow-lg p-2 bg-blue-400 active:mr-1 active:ml-3 active:shadow-none dark:border-darkComponent">
-          <ThumbUpIcon className="inline-block h-5 w-5 m-1 text-mainWhite dark:text-mainBlack " />
+        <button
+          className={
+            (isLiked
+              ? 'bg-cyan-700 shadow-inner border-slate-400'
+              : 'bg-blue-400 hover:shadow-lg border-white') +
+            ' border-4 m-2  rounded-xl p-2 active:mr-1 active:ml-3 active:shadow-none dark:border-darkComponent'
+          }
+          disabled={isLiked}
+          onClick={() => clickLikeHandler()}
+        >
+          <ThumbUpIcon
+            className={
+              (isLiked
+                ? 'text-slate-400'
+                : 'text-mainWhite dark:text-mainBlack') +
+              ' inline-block h-5 w-5 m-1 text-mainWhite dark:text-mainBlack '
+            }
+          />
           추천
         </button>
-        <button className=" border-4 m-2 border-white rounded-xl shadow-lg p-2 bg-red-400 active:mr-1 active:ml-3 active:shadow-none dark:border-darkComponent">
-          <ThumbDownIcon className="inline-block h-5 w-5 m-1 text-mainWhite dark:text-mainBlack" />
+        <button
+          className={
+            (isDisliked
+              ? 'bg-red-900 shadow-inner border-slate-400'
+              : 'bg-red-400 hover:shadow-lg border-white') +
+            ' border-4 m-2  rounded-xl p-2 active:mr-1 active:ml-3 active:shadow-none dark:border-darkComponent'
+          }
+          disabled={false /*isDisliked*/}
+          onClick={() => clickDislikeHandler()}
+        >
+          <ThumbDownIcon
+            className={
+              (isDisliked
+                ? 'text-slate-400'
+                : 'text-mainWhite dark:text-mainBlack') +
+              ' inline-block h-5 w-5 m-1 text-mainWhite dark:text-mainBlack '
+            }
+          />
           비추천
         </button>
       </div>
