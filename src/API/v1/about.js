04e378ada82692d0e8a74e26358c11855be1bd55
kej <publdaze@naver.com>
2022-02-21 20:11:32 +0900

    feat) About(intro) page API 적용
    - intro API 적용
    - 불필요한 주석 제거
    
    #18

--- src/API/v1/about.js
+++ src/API/v1/about.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-// Dice
 async function getIntroInfo() {
   const options = {
     method: 'GET',
--- src/page/About/Component/Intro.jsx
+++ src/page/About/Component/Intro.jsx
@@ -1,35 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import AboutBgImg from 'assets/img/AboutBgImg.png';
 
+// API
+import aboutAPI from 'API/v1/about';
+
 export default function Intro() {
+  const [introInfo, setIntroInfo] = useState([
+    {
+      id: null,
+      title: null,
+      type: null,
+      subtitleImageResults: [
+        {
+          id: null,
+          subtitle: null,
+          staticWriteTitleId: null,
+          thumbnail: {
+            id: null,
+          },
+          displayOrder: null,
+          staticWriteContentResults: [
+            {
+              id: null,
+              content: null,
+              staticWriteSubtitleImageId: null,
+              displayOrder: null,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  useEffect(() => {
+    aboutAPI.getIntroInfo().then((data) => {
+      if (data.success) {
+        setIntroInfo(data.list);
+      }
+    });
+  }, []);
+
   const pageTitle = 'KEEPER';
-  const sectionTitle = '키퍼(KEEPER) 소개글';
-  // ANCHOR 내용 받아와보고 content 형식 정할 예정
-  // ANCHOR 반응형으로 할 때 개행 처리
-  /* const content =
-    "'지키다'라는 의미를 가진 단어 'KEEP'에서 착안하여, 정보보호에 관한 \
-연구를 진행하고 \n그 성과를 공유하기 위해 만들어진 동아리입니다. \
-\n \n 전국의 많은 정보보안 동아리들과 교류하고 있으며, \n 습득한 \
-지식을 시험해 볼 수 있는 다양한 해킹방어대회에 참여하고 있습니다. \
-\n \n 정보보안은 학과 커리큘럼에서 굉장히 늦게, 조금 포함되어 있고 \
-\n 인터넷에 자료도 많지 않아 혼자 공부하기 어려운 부분이 있습니다. \
-\n \n KEEPER에 가입하시면 선배, 후배, 동기들과 다 같이 정보보안을 \
-공부할 수 있습니다. \n 1학기 초에 열리는 오픈 세미나에서 신입 \
-회원을 모집합니다. \n 특별한 재능이 있지 않아도, 성실하고 의지 \
-넘치는 분이면 환영합니다! \n \n"; */
+  const sectionTitle = introInfo[0].title;
 
-  const content = `'지키다'라는 의미를 가진 단어 'KEEP'에서 착안하여, 정보보호에 관한 연구를 진행하고
-그 성과를 공유하기 위해 만들어진 동아리입니다.
-
-전국의 많은 정보보안 동아리들과 교류하고 있으며,
-습득한 지식을 시험해 볼 수 있는 다양한 해킹방어대회에 참여하고 있습니다. 
-
-정보보안은 학과 커리큘럼에서 굉장히 늦게, 조금 포함되어 있고
-인터넷에 자료도 많지 않아 혼자 공부하기 어려운 부분이 있습니다. 
-
-KEEPER에 가입하시면 선배, 후배, 동기들과 다 같이 정보보안을 공부할 수 있습니다.
-1학기 초에 열리는 오픈 세미나에서 신입 회원을 모집합니다.
-특별한 재능이 있지 않아도, 성실하고 의지 넘치는 분이면 환영합니다!`;
+  const content =
+    introInfo[0].subtitleImageResults[0].staticWriteContentResults[0].content;
 
   const boss = '이창율'; //TODO 회장 직위 가진 사람 받아오기
   const supervisors = `
@@ -48,7 +63,7 @@ KEEPER에 가입하시면 선배, 후배, 동기들과 다 같이 정보보안�
             <h2 className="pb-6 lg:pb-10 text-2xl font-extrabold tracking-tight text-black dark:text-mainYellow">
               {sectionTitle}
             </h2>
-            <div className="whitespace-pre-wrap px-2 lg:px-4 text-base text-black dark:text-white">
+            <div className="md:whitespace-pre-wrap px-2 lg:px-4 text-base text-black dark:text-white">
               {content}
               <br />
               {supervisors}
