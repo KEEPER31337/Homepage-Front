459afbba5e22d5c16197a432aa382d0e4b5a0775
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2022-02-21 18:01:08 +0900

    Design) add dark mode and change color set

--- src/page/Home/Component/Trends.jsx
+++ src/page/Home/Component/Trends.jsx
@@ -78,13 +78,13 @@ const posts = [
   
   export default function Trends() {
     return (
-      <div className="relative bg-gray-50 h-auto pt-16 pb-4 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="relative bg-gray-50 dark:bg-neutral-900 h-auto pt-16 pb-4 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
-          <div className="bg-white h-1/5 sm:h-2/3" />
+          <div className="bg-mainWhite dark:bg-mainBlack h-1/5 sm:h-2/3" />
         </div>
         <div className="relative max-w-7xl mx-auto">
           <div className="text-center">
-            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">트렌딩</h2>
+            <h2 className="text-3xl tracking-tight font-extrabold sm:text-4xl ">트렌딩</h2>
           </div>
           <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-4 lg:max-w-none">
             {posts.map((post) => (
@@ -92,16 +92,15 @@ const posts = [
                 <div className="flex-shrink-0">
                   <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
                 </div>
-                <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1 bg-mainWhite dark:bg-mainBlack p-6 flex flex-col justify-between">
                   <div className="flex-1">
-                    <p className="text-sm font-medium text-indigo-600">
+                    <p className="text-sm font-medium text-mainYellow">
                       <a href={post.category.href} className="hover:underline">
                         {post.category.name}
                       </a>
                     </p>
                     <a href={post.href} className="block mt-2">
-                      <p className="text-xl font-semibold text-gray-900">{post.title}</p>
-                      {/* <p className="mt-3 text-base text-gray-500">{post.description}</p> */}
+                      <p className="text-xl font-semibold dark:text-mainWhite">{post.title}</p>
                     </a>
                   </div>
                   <div className="mt-6 flex items-center">
@@ -112,7 +111,7 @@ const posts = [
                       </a>
                     </div>
                     <div className="ml-3">
-                      <p className="text-sm font-medium text-gray-900">
+                      <p className="text-sm font-medium">
                         <a href={post.author.href} className="hover:underline">
                           {post.author.name}
                         </a>
--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,29 +1,34 @@
 import React, { useState } from 'react';
-import Notice from './src/Notice'
-import Trends from './src/Trends';
-import Latest from './src/Latest';
+import { connect } from 'react-redux';
+
 import './style/height.css';
+
+import Notice from './Component/Notice'
+import Trends from './Component/Trends';
+import Latest from './Component/Latest';
+
 import Logo from 'assets/img/keeper_logo.png';
-import UpArrow from 'assets/img/up_arrow.png';
-import DownArrow from 'assets/img/down_arrow.png';
+
+import GrayUpArrow from 'assets/img/gray-up-arrow.png';
+import GrayDownArrow from 'assets/img/gray-down-arrow.png';
 
 
 const Home = () => {
   const [visible, setVisible] = useState(true);
   return (
-    <div className="home">
+    <div className="bg-mainWhite dark:bg-mainBlack text-mainBlack dark:text-mainWhite">
         {visible && FirstPage(visible, setVisible)}
         {SecondPage(visible, setVisible)}
     </div>
-  );
-};
+  )
+}
 
 function FirstPage(visible, setVisible) {
   return (
   <div id="main-first-page" className="grid content-around">
     {<Notice />}
     <img className="m-auto" alt="keeper_logo" src={Logo}></img>
-    <div className="text-xl font-semibold text-gray-900 text-center">
+    <div className="text-xl font-semibold text-center tracking-wide dark:text-mainYellow">
       '지키다'라는 의미를 가진 단어 'KEEP'에서 착안하여, <br></br> 
       정보보호에 관한 연구를 진행하고 그 성과를 공유하기 위해 만들어진 동아리입니다.
     </div>
@@ -45,7 +50,7 @@ function SecondPage(visible, setVisible) {
 function Arrow(visible, setVisible) {
   return (
     <a href="#">
-      <img className="m-auto w-16 h-16" src={visible ? UpArrow : DownArrow} onClick={
+      <img className="m-auto w-16 h-16" src={visible ? GrayUpArrow : GrayDownArrow} onClick={
         () => {
           setVisible(!visible);
         }
@@ -54,5 +59,8 @@ function Arrow(visible, setVisible) {
   )
 }
 
+const mapStateToProps = (state) => {
+  return { state };
+};
 
-export default Home;
+export default connect(mapStateToProps)(Home);
