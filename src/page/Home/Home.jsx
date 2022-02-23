87b4564e10fd1b355a8da61130839ed6f18206f5
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2022-02-23 14:55:20 +0900

    design) modify size

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -27,8 +27,8 @@ function FirstPage(visible, setVisible) {
   return (
   <div id="main-first-page" className="grid content-around">
     {<Notice />}
-    <img className="m-auto" alt="keeper_logo" src={Logo}></img>
-    <div className="font-semibold text-center tracking-wide dark:text-mainYellow text-sm sm:text-xl lg:text-xl">
+    <img className="m-auto w-3/4 lg:w-fit " alt="keeper_logo" src={Logo}></img>
+    <div className="font-semibold text-center tracking-wide dark:text-mainYellow text-lg sm:text-xl lg:text-xl">
       '지키다'라는 의미를 가진 단어 'KEEP'에서 착안하여, <br></br> 
       정보보호에 관한 연구를 진행하고 그 성과를 공유하기 위해 만들어진 동아리입니다.
     </div>
