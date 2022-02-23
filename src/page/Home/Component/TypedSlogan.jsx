f6e9960db2910cf8647963644da1760ceaf8f667
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2022-02-23 21:15:45 +0900

    refactoring) delete unused code

--- src/page/Home/Component/TypedSlogan.jsx
+++ src/page/Home/Component/TypedSlogan.jsx
@@ -1,3 +1,5 @@
+// Using typed.js for typing animation
+
 import React from 'react';
 import Typed from 'typed.js';
 
@@ -13,7 +15,6 @@ const TypedSlogan = () => {
             "'지키다'라는 의미를 가진 단어 'KEEP'에서 착안하여, \n 정보보호에 관한 연구를 진행하고 그 성과를 공유하기 위해 만들어진 동아리입니다."
         ],
         typeSpeed: 50,
-        backSpeed: 50,
     };
     
     // elRef refers to the <span> rendered below
