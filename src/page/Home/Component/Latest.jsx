2246a708ab6bd9595a30ab4e066186071dc53025
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2022-02-24 16:26:47 +0900

    feat) add aos (scroll fade in) - delete test code (console.log)

--- src/page/Home/Component/Latest.jsx
+++ src/page/Home/Component/Latest.jsx
@@ -30,7 +30,7 @@ const posts = [
 
 export default function Latest({ postList }) {
   return (
-    <div className="relative bg-gray-50 dark:bg-neutral-900 h-auto pt-16 pb-4 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div className="relative bg-gray-50 dark:bg-neutral-900 h-auto pt-16 pb-4 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8" data-aos="fade-in">
       <div className="absolute inset-0">
         <div className="bg-mainWhite dark:bg-mainBlack h-2/3" />
       </div>
@@ -98,7 +98,7 @@ export default function Latest({ postList }) {
             ))}
           </div>
         </ScrollContainer>
-      </div>
+      </div>  
     </div>
   );
 }
--- src/page/Home/Component/SecondPage.jsx
+++ src/page/Home/Component/SecondPage.jsx
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from 'react';
 import '../style/height.css';
 import Trends from './Trends';
 import Latest from './Latest';
-import GrayUpArrow from 'assets/img/gray-up-arrow.png';
 
 // API
 import homeAPI from 'API/v1/home';
@@ -29,17 +28,6 @@ const SecondPage = ({ goToFirst, visibleArrow }) => {
 
   return (
     <div id="main-second-page" className="pt-16">
-      {!visibleArrow && (
-        <a href="#">
-          <img
-            className="m-auto w-16 h-16"
-            src={GrayUpArrow}
-            onClick={() => {
-              goToFirst();
-            }}
-          ></img>
-        </a>
-      )}
       <Trends postList={trendPostList} />
       <Latest postList={latestPostList} />
     </div>
