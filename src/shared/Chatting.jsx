9e4cda230c2f14b0203a9291b2ec5281656c4c58
goo-gy <lkukh17@naver.com>
2022-03-09 23:23:56 +0900

    feat) 로그인 사용자만 채팅 이용
    - 로그인 사용자만 채팅 사용할 수 있도록
    
    #84

--- src/shared/Chatting.jsx
+++ src/shared/Chatting.jsx
@@ -1,11 +1,14 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { ChatIcon } from '@heroicons/react/solid';
+import { connect } from 'react-redux';
 
 //local
-import ChatModel from './Component/ChatModal';
+import ChatModal from './Component/ChatModal';
+import authAPI from 'API/v1/auth';
 
-const Chatting = () => {
+const Chatting = ({ member }) => {
   const [open, setOpen] = useState(false);
+  const [auth, setAuth] = useState(false);
 
   const handleOpen = () => {
     setOpen(true);
@@ -15,24 +18,45 @@ const Chatting = () => {
     setOpen(false);
   };
 
+  useEffect(() => {
+    if (member.token) {
+      authAPI.getAuth({ token: member.token }).then((data) => {
+        if (data.success) {
+          setAuth(true);
+        }
+      });
+    } else {
+      // TODO : out from the room
+      setAuth(false);
+    }
+  }, [member]);
   return (
     <>
-      <div className="fixed bottom-5 right-5">
-        <button
-          className={`${
-            open ? 'invisible' : 'visible'
-          } rounded-md px-5 py-1 font-semibold text-mainBlack bg-mainYellow hover:bg-pointYellow`}
-          onClick={handleOpen}
-        >
-          <ChatIcon className="h-8 w-8" />
-          Chat
-        </button>
-      </div>
-      <div className="fixed bottom-20 right-5">
-        <ChatModel visible={open} handleClose={handleClose} />
-      </div>
+      {auth ? (
+        <>
+          <div className="fixed bottom-5 right-5">
+            <button
+              className={`${
+                open ? 'invisible' : 'visible'
+              } rounded-md px-5 py-1 font-semibold text-mainBlack bg-mainYellow hover:bg-pointYellow`}
+              onClick={handleOpen}
+            >
+              <ChatIcon className="h-8 w-8" />
+              Chat
+            </button>
+          </div>
+          <div className="fixed bottom-20 right-5">
+            <ChatModal visible={open} handleClose={handleClose} />
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
+const mapStateToProps = (state) => {
+  return { member: state.member };
+};
 
-export default Chatting;
+export default connect(mapStateToProps)(Chatting);
