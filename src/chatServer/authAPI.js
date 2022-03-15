38ce741cf2025e6c0d40e33cb4d8153366e5c202
goo-gy <lkukh17@naver.com>
2022-03-15 11:25:33 +0900

    fix : 수정된 API 반영
    #86

--- src/chatServer/authAPI.js
+++ src/chatServer/authAPI.js
@@ -2,11 +2,10 @@ const axios = require('axios');
 require('dotenv').config();
 
 const API_URL = process.env.REACT_APP_API_URL;
-console.log('url', API_URL);
 async function getAuth({ token }) {
   const options = {
     method: 'GET',
-    url: API_URL + '/v1/member',
+    url: API_URL + '/v1/members',
     headers: {
       Authorization: token,
     },
