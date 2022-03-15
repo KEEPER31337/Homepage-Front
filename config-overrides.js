fff07862a3e7c418300ed1359072befbfd0ab80c
goo-gy <lkukh17@naver.com>
2022-03-15 19:07:09 +0900

    feat : production에서 console.log 제거
    - readme 수정
    - react-app-rewired와 customize-cra 이용
    - .env에서 mode 설정하여 production에서는 console.log 제거
    
    #5, #52

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,11 @@
-module.exports = function override(config, env) {
-  return config;
+const { override, addBabelPlugins } = require('customize-cra');
+
+const isProduction = process.env.REACT_APP_MODE === 'production';
+
+module.exports = {
+  webpack: isProduction
+    ? override(addBabelPlugins('transform-remove-console'))
+    : function override(config, env) {
+        return config;
+      },
 };
