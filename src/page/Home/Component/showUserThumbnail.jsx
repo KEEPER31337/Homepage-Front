b18ee2b9a820f340b8418477a79d3816ad09c3df
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2022-03-16 19:27:37 +0900

    design) change user default thumbnail image

--- src/page/Home/Component/showUserThumbnail.jsx
+++ src/page/Home/Component/showUserThumbnail.jsx
@@ -1,10 +1,10 @@
-import KeyLogo from 'assets/img/keeper_logo_key.png';
+import DefaultUserThumbnail from 'assets/img/default_thumbnail_square_clear.png';
 
 export default function showUserThumbnail(thumbnailPath) {
     if (thumbnailPath != null) {
         return thumbnailPath;
     }
     else {
-        return KeyLogo;
+        return DefaultUserThumbnail;
     }
 }
