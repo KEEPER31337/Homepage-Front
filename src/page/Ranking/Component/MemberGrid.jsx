de172d6ba6e8b59599fd22f64abf91bf778f743f
goo-gy <lkukh17@naver.com>
2022-03-16 17:48:56 +0900

    fix : image 업데이트
    - image가져오는 부분 API 수정에 맞게 업데이트
    - 기본 이미지 등록

--- src/page/Ranking/Component/MemberGrid.jsx
+++ src/page/Ranking/Component/MemberGrid.jsx
@@ -1,15 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
+//local
+import memberImage from 'assets/img/memberCircle.svg';
 // API
 import memberAPI from 'API/v1/member';
-
-const thumbnailList = [
-  'https://avatars.githubusercontent.com/u/23546441?s=400&u=db7abf2929e5518c12189034dc3fed9bda94f0a6&v=4',
-  'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-  'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-  'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-];
 
 const MemberGrid = ({ member }) => {
   const [memberList, setMemberList] = useState([]);
@@ -30,9 +25,7 @@ const MemberGrid = ({ member }) => {
               <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={
-                    member.thumbnail
-                      ? member.thumbnail
-                      : thumbnailList[index % 4]
+                    member.thumbnailPath ? member.thumbnailPath : memberImage
                   }
                   alt="profile"
                   className="w-full h-full object-center object-cover group-hover:opacity-75"
