5fa3dbc4bab3e47bacb7c50e4e2dea5868a081ec
pipisebastian <yrt7998@pusan.ac.kr>
2022-03-31 22:55:13 +0900

    fix) 모바일 버전 - 로그인, 회원가입 버튼 바꾸기
    - 모바일 버전 - 로그인, 회원가입 버튼 바꾸기
    
    #19

--- src/shared/Component/SignInBoxMobile.jsx
+++ src/shared/Component/SignInBoxMobile.jsx
@@ -6,15 +6,15 @@ const SignInBoxMobile = () => {
   return (
     <div>
       <Popover.Button className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-mainYellow hover:bg-pointYellow">
-        <Link to="signup" className="w-full">
-          회원가입
+        <Link to="signin" className="w-full">
+          로그인
         </Link>
       </Popover.Button>
       <p className="mt-6 text-center text-base font-medium text-mainBlack dark:text-mainWhite">
-        Existing account?{' '}
+        계정이 없으신가요?{' '}
         <Popover.Button className="text-base text-mainYellow hover:text-pointYellow">
-          <Link to="signin" className="w-full">
-            로그인
+          <Link to="signup" className="w-full">
+            회원가입
           </Link>
         </Popover.Button>
       </p>
