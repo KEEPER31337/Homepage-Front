a9b30ea2822ea03ea4117eca92e9b46ddce0952d
amaran-th <songsy405@gmail.com>
2022-04-21 12:56:35 +0900

    style:스터디 페이지 UI 개선 - 가로 크기 규격 정함(5xl, 3xl) - 컨텐츠 레이아웃을 grid로 변경 - 사이드바를 sticky 형식으로 변경 및 버튼 UI 변경(모바일 UI도 개선) - 몇몇 margin, padding을 gap, space로 변경 - 모바일)외부링크의 아이콘을 가로로 정렬하여 보이게 함
    #109

--- src/page/Study/Components/AboutCard.jsx
+++ src/page/Study/Components/AboutCard.jsx
@@ -10,7 +10,7 @@ const AboutCard = ({ study, setCurrentStudy, state }) => {
   return (
     <div
       name="상세보기"
-      className="bg-[rgb(255,209,90)] shadow overflow-hidden sm:rounded-lg my-5 border-2 border-mainYellow dark:text-mainWhite"
+      className="bg-[rgb(255,209,90)] shadow-md overflow-hidden sm:rounded-lg dark:text-mainWhite"
     >
       <div className="bg-transparent px-3 py-2 sm:rounded-t-lg sm:px-6">
         <div
@@ -26,33 +26,30 @@ const AboutCard = ({ study, setCurrentStudy, state }) => {
           </div>
           {study.headMember.id == myId ? (
             <>
-              <div className="ml-4 mt-2 flex-shrink-0 hidden sm:block">
+              <div className="ml-4 mt-2 flex-shrink-0">
                 <button
+                  name="pc 수정버튼"
                   type="button"
-                  className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none dark:bg-gray-500 dark:text-slate-100 dark:border-gray-600"
+                  className="hidden sm:inline-flex shadow-inner ml-3 relative items-center px-4 py-2 border border-pointYellow shadow-sm text-sm font-bold rounded-md text-pointYellow hover:shadow-md focus:outline-none dark:bg-darkComponent dark:text-slate-100 dark:border-gray-600"
                   onClick={(e) => {
                     setCurrentStudy(study);
                   }}
                 >
                   <CogIcon
-                    className="-ml-1 mr-2 h-5 w-5 text-gray-400 dark:text-mainWhite"
+                    className="-ml-1 mr-2 h-5 w-5 dark:text-mainWhite"
                     aria-hidden="true"
                   />
                   <span>수정하기</span>
-                </button>
-              </div>
-              <div className="ml-4 mt-2 flex-shrink-0 sm:hidden">
+                </button>{' '}
                 <button
+                  name="모바일 수정버튼"
                   type="button"
-                  className="ml-3 relative inline-flex items-center p-1 shadow-sm"
+                  className="sm:hidden border border-pointYellow rounded-full text-pointYellow ml-3 relative inline-flex items-center p-1 shadow-inner"
                   onClick={(e) => {
                     setCurrentStudy(study);
                   }}
                 >
-                  <CogIcon
-                    className="h-6 w-6 text-gray-400 dark:text-mainWhite"
-                    aria-hidden="true"
-                  />
+                  <CogIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
               </div>
             </>
@@ -67,7 +64,7 @@ const AboutCard = ({ study, setCurrentStudy, state }) => {
             <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
               스터디 소개
             </dt>
-            <dd className="mt-1 text-sm text-gray-900 dark:text-mainWhite">
+            <dd className="border-l-2 border-mainYellow px-2 mt-1 text-sm text-gray-900 dark:text-mainWhite">
               {study.information}
             </dd>
           </div>
@@ -76,7 +73,7 @@ const AboutCard = ({ study, setCurrentStudy, state }) => {
               스터디장
             </dt>
             <dd className="mt-1 text-sm text-gray-900 dark:text-mainWhite">
-              <span className="inline-block border border-gray-300 min-w-[4em] py-1 pl-2 pr-5 m-[1px] text-sm rounded-full">
+              <span className="inline-block border border-gray-300 min-w-[4em] py-1 px-2 m-[1px] text-sm text-center rounded-full">
                 {study.headMember?.nickName}
               </span>
             </dd>
@@ -85,7 +82,7 @@ const AboutCard = ({ study, setCurrentStudy, state }) => {
             </dt>
             <dd className="mt-1 text-sm text-gray-900 dark:text-mainWhite">
               {study.memberList?.map((member) => (
-                <span className="inline-block border border-gray-300 min-w-[4em] py-1 pl-2 pr-5 m-[1px] text-sm rounded-full">
+                <span className="inline-block border border-gray-300 min-w-[4em] py-1 px-2 m-[1px] text-sm text-center rounded-full">
                   {member.nickName}
                 </span>
               ))}
@@ -100,11 +97,11 @@ const AboutCard = ({ study, setCurrentStudy, state }) => {
             />
           </div>
           {study.gitLink || study.noteLink || study.etcLink ? (
-            <div className="sm:col-span-2">
+            <div className="w-1/2 sm:w-full sm:col-span-2">
               <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
                 외부 링크
               </dt>
-              <dd className="mt-1 text-sm text-gray-900 dark:text-mainWhite">
+              <dd className="hidden sm:block mt-1 text-sm text-gray-900 dark:text-mainWhite">
                 <ul
                   role="list"
                   className="border border-gray-200 rounded-md divide-y divide-gray-200 dark:border-gray-600 dark:divide-gray-600"
@@ -114,7 +111,7 @@ const AboutCard = ({ study, setCurrentStudy, state }) => {
                       <div className="w-0 flex-1 flex items-center">
                         <img
                           src={require('assets/img/icons/github.png')}
-                          className="flex-shrink-0 h-5 w-5"
+                          className="bg-white rounded-full ring-1 ring-white flex-shrink-0 h-5 w-5"
                         />
                         <span className="ml-2 flex-1 w-0 truncate">
                           <a href={study.gitLink} target="_blank">
@@ -161,6 +158,38 @@ const AboutCard = ({ study, setCurrentStudy, state }) => {
                     ''
                   )}
                 </ul>
+              </dd>
+              <dd className="mt-1 border rounded-lg w-full px-5 p-2 flex sm:hidden dark:border-gray-600">
+                <span className="flex-1 flex justify-center">
+                  {study.gitLink ? (
+                    <img
+                      src={require('assets/img/icons/github.png')}
+                      className="bg-white rounded-full ring-1 ring-white flex-shrink-0 h-7 w-7"
+                    />
+                  ) : (
+                    ''
+                  )}
+                </span>
+                <span className="flex-1 flex justify-center">
+                  {study.noteLink ? (
+                    <img
+                      src={require('assets/img/icons/notion.png')}
+                      className="flex-shrink-0 h-7 w-7"
+                    />
+                  ) : (
+                    ''
+                  )}
+                </span>
+                <span className="flex-1 flex justify-center">
+                  {study.etcLink ? (
+                    <PaperClipIcon
+                      className="flex-shrink-0 h-7 w-7 text-gray-400 dark:text-gray-400"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    ''
+                  )}
+                </span>
               </dd>
             </div>
           ) : (
--- src/page/Study/Components/StudyList.jsx
+++ src/page/Study/Components/StudyList.jsx
@@ -17,7 +17,6 @@ const StudyList = ({
   changeFlag,
   currentYear,
   currentSeason,
-  setCurrentSeason,
   setCurrentStudy,
   state,
 }) => {
@@ -45,54 +44,8 @@ const StudyList = ({
   var link =
     'https://enormous-button-c5d.notion.site/2021-7a9e28c746934f22863f7077fec061da';
   return (
-    <div className="">
-      <div name="시즌 태그들" className="text-sm mt-5 md:text-xl">
-        <button
-          className={
-            (currentSeason == 1
-              ? 'border-mainYellow bg-mainYellow'
-              : 'hover:bg-slate-100 dark:border-darkComponent dark:hover:bg-gray-800') +
-            ' border-x-2 border-t-2  inline-block w-[5.5em] p-1 px-2 rounded-t-2xl '
-          }
-          onClick={() => setCurrentSeason(1)}
-        >
-          1학기
-        </button>
-        <button
-          className={
-            (currentSeason == 2
-              ? 'border-mainYellow bg-mainYellow'
-              : 'hover:bg-slate-100 dark:border-darkComponent dark:hover:bg-gray-800') +
-            ' border-x-2 border-t-2  inline-block w-[5.5em] p-1 px-2 rounded-t-2xl '
-          }
-          onClick={() => setCurrentSeason(2)}
-        >
-          여름방학
-        </button>
-        <button
-          className={
-            (currentSeason == 3
-              ? 'border-mainYellow bg-mainYellow'
-              : 'hover:bg-slate-100 dark:border-darkComponent dark:hover:bg-gray-800') +
-            ' border-x-2 border-t-2  inline-block w-[5.5em] p-1 px-2 rounded-t-2xl '
-          }
-          onClick={() => setCurrentSeason(3)}
-        >
-          2학기
-        </button>
-        <button
-          className={
-            (currentSeason == 4
-              ? 'border-mainYellow bg-mainYellow'
-              : 'hover:bg-slate-100 dark:border-darkComponent dark:hover:bg-gray-800') +
-            ' border-x-2 border-t-2  inline-block w-[5.5em] p-1 px-2 rounded-t-2xl '
-          }
-          onClick={() => setCurrentSeason(4)}
-        >
-          겨울방학
-        </button>
-      </div>
-      <div className="border-2 max-w-[80rem] min-h-[60vh] border-mainYellow rounded-b-lg rounded-tr-lg bg-gray-50 w-full px-5 md:w-[80vw] dark:bg-darkPoint">
+    <>
+      <div className="space-y-4">
         {/*link ? (
             <p className="border-b border-x bg-mainWhite my-3 p-3 rounded-lg dark:border-gray-700 dark:bg-mainBlack">
               노션 링크 :
@@ -119,7 +72,7 @@ const StudyList = ({
           <div>현재 등록된 스터디가 없습니다.</div>
         )}
       </div>
-    </div>
+    </>
   );
 };
 const mapStateToProps = (state, OwnProps) => {
