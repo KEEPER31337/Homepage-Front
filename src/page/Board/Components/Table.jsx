2f6805b7cb4ffda25c9f3ce5c30eca845b8cc7fb
amaran-th <songsy405@gmail.com>
2022-06-01 06:16:36 +0900

    refactor: 게시글 목록
    - 게시글목록의 행 및 카드를 함수화함
    
    #108

--- src/page/Board/Components/Table.jsx
+++ src/page/Board/Components/Table.jsx
@@ -28,6 +28,100 @@ const Table = ({
     if (id == currentId) return 'text-mainYellow  rounded-lg';
     return;
   };
+  const row = (board, index, isN) => {
+    //isN은 공지글인지 일반 글인지 여부(true=>공지글, false=>일반 글)
+    return (
+      <tr
+        key={board.id}
+        className={
+          (isN
+            ? 'bg-gray-100 dark:bg-gray-900 border-b-2 hover:bg-gray-200 hover:shadow-lg dark:hover:bg-darkComponent dark:border-darkComponent '
+            : ' border-b-2 hover:bg-gray-50 hover:shadow-lg dark:hover:bg-darkComponent dark:border-darkComponent ') +
+          getCurrentBoard(board.id, postId)
+        }
+      >
+        {/*console.log(board)*/}
+        <td className="w-[3em] border-r border-divisionGray text-center dark:border-darkComponent">
+          {isN ? '공지' : MAX_POSTS * (currentPage - 1) + index + 1}
+        </td>
+        <td className="p-2 max-w-0 dark:border-darkComponent">
+          <Link
+            to={`/post/${categoryId}/${board.id}`}
+            onClick={(e) => {
+              if (
+                board.isSecret &&
+                board.writerId != state.member?.memberInfo?.id
+              )
+                linkHandler(e, board);
+            }}
+          >
+            <div className=" w-full flex items-center">
+              <p className="truncate text-md mr-1">
+                {board.isSecret ? (
+                  <LockClosedIcon className="inline-block h-5 w-5 m-1 text-slate-400 " />
+                ) : (
+                  ''
+                )}
+                <strong className={board.isSecret ? 'text-slate-400' : ''}>
+                  {board.title}
+                </strong>
+              </p>
+              <div className="flex-1">
+                {board.thumbnail ? (
+                  <PhotographIcon className="inline-block h-5 w-5 m-1 text-slate-500 " />
+                ) : (
+                  ''
+                )}
+                {board.files ? (
+                  <DocumentTextIcon className="inline-block h-5 w-5 text-slate-500" />
+                ) : (
+                  ''
+                )}
+                {board.commentCount != 0 ? (
+                  <strong className="text-mainYellow">
+                    {'(' + board.commentCount + ')'}
+                  </strong>
+                ) : (
+                  ''
+                )}
+                {isNewPost(board.registerTime) ? (
+                  <strong
+                    className={
+                      (isN ? 'dark:text-mainBlack' : '') +
+                      ' inline-block rounded-full w-5 h-5 align-middle text-center text-xs my-1 pb-1 bg-red-500 shadow-lg border-2 border-red-200 text-mainWhite'
+                    }
+                  >
+                    N
+                  </strong>
+                ) : (
+                  ''
+                )}
+              </div>
+            </div>
+            <p className="mt-2 text-xs md:hidden">
+              글쓴이 : <strong>{board.writer} </strong>| 작성일시 :
+              <strong>{getDateWithFormat(board.registerTime)} </strong>|{' '}
+              <span className="inline-block">
+                조회수 : <strong>{board.visitCount} </strong>
+              </span>
+            </p>
+          </Link>
+        </td>
+
+        <td className="text-center dark:border-darkComponent hidden md:table-cell">
+          {board.writer}
+        </td>
+        <td className="text-center dark:border-darkComponent hidden md:table-cell">
+          {/*getDateWithFormat(board.registerTime)
+          <br />*/}
+          {getDiffTimeWithFormat(board.registerTime)}
+        </td>
+        <td className="text-center dark:border-darkComponent hidden md:table-cell">
+          {board.visitCount}
+        </td>
+      </tr>
+    );
+  };
 
   return (
     <table className="w-full">
@@ -51,170 +145,8 @@ const Table = ({
         </tr>
       </thead>
       <tbody>
-        {notices?.map((board, index) => (
-          <tr
-            key={index}
-            className={
-              'bg-gray-100 dark:bg-gray-900 border-b-2 hover:bg-gray-200 hover:shadow-lg dark:hover:bg-darkComponent dark:border-darkComponent ' +
-              getCurrentBoard(board.id, postId)
-            }
-          >
-            <td className="border-r border-divisionGray text-center dark:border-darkComponent">
-              공지
-            </td>
-            <td className="p-2 max-w-0 dark:border-darkComponent">
-              <Link
-                to={`/post/${categoryId}/${board.id}`}
-                onClick={(e) => {
-                  if (
-                    board.isSecret &&
-                    board.writerId != state.member?.memberInfo?.id
-                  )
-                    linkHandler(e, board);
-                }}
-              >
-                <div className=" w-full flex items-center">
-                  <p className="truncate text-md ">
-                    <strong className={board.isSecret ? 'text-slate-400' : ''}>
-                      {board.title}
-                    </strong>
-                  </p>
-
-                  <div className="flex-1">
-                    {board.thumbnail ? (
-                      <PhotographIcon className="inline-block h-5 w-5 m-1 text-slate-500 " />
-                    ) : (
-                      ''
-                    )}
-                    {board.files && board.files.length != 0 ? (
-                      <DocumentTextIcon className="inline-block h-5 w-5 text-slate-500" />
-                    ) : (
-                      ''
-                    )}
-                    {board.commentCount != 0 ? (
-                      <strong className="text-mainYellow">
-                        {'(' + board.commentCount + ')'}
-                      </strong>
-                    ) : (
-                      ''
-                    )}
-                    {isNewPost(board.registerTime) ? (
-                      <strong className="inline-block rounded-full w-5 h-5 align-middle text-center text-xs m-1 pb-1 bg-red-500 shadow-lg border-2 border-red-200 text-mainWhite dark:text-mainBlack">
-                        N
-                      </strong>
-                    ) : (
-                      ''
-                    )}
-                  </div>
-                </div>
-                <p className="mt-2 text-xs md:hidden">
-                  글쓴이 : <strong>{board.writer} </strong>| 작성일시 :
-                  <strong>{getDateWithFormat(board.registerTime)} </strong>|{' '}
-                  <span className="inline-block">
-                    조회수 : <strong>{board.visitCount} </strong>
-                  </span>
-                </p>
-              </Link>
-            </td>
-
-            <td className="text-center dark:border-darkComponent hidden md:table-cell">
-              {board.writer}
-            </td>
-            <td className="text-center dark:border-darkComponent hidden md:table-cell">
-              {/*getDateWithFormat(board.registerTime)
-                <br />*/}
-              {getDiffTimeWithFormat(board.registerTime)}
-            </td>
-            <td className="text-center dark:border-darkComponent hidden md:table-cell">
-              {board.visitCount}
-            </td>
-          </tr>
-        ))}
-        {/*=============================================================================================*/}
-        {boards?.map((board, index) => (
-          <tr
-            key={board.id}
-            className={
-              ' border-b-2 hover:bg-gray-50 hover:shadow-lg dark:hover:bg-darkComponent dark:border-darkComponent ' +
-              getCurrentBoard(board.id, postId)
-            }
-          >
-            {/*console.log(board)*/}
-            <td className="w-[3em] border-r border-divisionGray text-center dark:border-darkComponent">
-              {MAX_POSTS * (currentPage - 1) + index + 1}
-            </td>
-            <td className="p-2 max-w-0 dark:border-darkComponent">
-              <Link
-                to={`/post/${categoryId}/${board.id}`}
-                onClick={(e) => {
-                  if (
-                    board.isSecret &&
-                    board.writerId != state.member?.memberInfo?.id
-                  )
-                    linkHandler(e, board);
-                }}
-              >
-                <div className=" w-full flex items-center">
-                  <p className="truncate text-md ">
-                    {board.isSecret ? (
-                      <LockClosedIcon className="inline-block h-5 w-5 m-1 text-slate-400 " />
-                    ) : (
-                      ''
-                    )}
-                    <strong className={board.isSecret ? 'text-slate-400' : ''}>
-                      {board.title}
-                    </strong>
-                  </p>
-                  <div className="flex-1">
-                    {board.thumbnail ? (
-                      <PhotographIcon className="inline-block h-5 w-5 m-1 text-slate-500 " />
-                    ) : (
-                      ''
-                    )}
-                    {board.files && board.files.length != 0 ? (
-                      <DocumentTextIcon className="inline-block h-5 w-5 text-slate-500" />
-                    ) : (
-                      ''
-                    )}
-                    {board.commentCount != 0 ? (
-                      <strong className="text-mainYellow">
-                        {'(' + board.commentCount + ')'}
-                      </strong>
-                    ) : (
-                      ''
-                    )}
-                    {isNewPost(board.registerTime) ? (
-                      <strong className="inline-block rounded-full w-5 h-5 align-middle text-center text-xs m-1 pb-1 bg-red-500 shadow-lg border-2 border-red-200 text-mainWhite">
-                        N
-                      </strong>
-                    ) : (
-                      ''
-                    )}
-                  </div>
-                </div>
-                <p className="mt-2 text-xs md:hidden">
-                  글쓴이 : <strong>{board.writer} </strong>| 작성일시 :
-                  <strong>{getDateWithFormat(board.registerTime)} </strong>|{' '}
-                  <span className="inline-block">
-                    조회수 : <strong>{board.visitCount} </strong>
-                  </span>
-                </p>
-              </Link>
-            </td>
-
-            <td className="text-center dark:border-darkComponent hidden md:table-cell">
-              {board.writer}
-            </td>
-            <td className="text-center dark:border-darkComponent hidden md:table-cell">
-              {/*getDateWithFormat(board.registerTime)
-                <br />*/}
-              {getDiffTimeWithFormat(board.registerTime)}
-            </td>
-            <td className="text-center dark:border-darkComponent hidden md:table-cell">
-              {board.visitCount}
-            </td>
-          </tr>
-        ))}
+        {notices?.map((board, index) => row(board, index, true))}
+        {boards?.map((board, index) => row(board, index, false))}
       </tbody>
     </table>
   );
