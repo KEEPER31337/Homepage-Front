03851c7afa7475ef5da97a42b8c228b30ffbfa24
amaran-th <songsy405@gmail.com>
2022-06-04 08:43:39 -0700

    fix:댓글 시차 수정
    #108, #156

--- src/page/Board/BoardUtil.jsx
+++ src/page/Board/BoardUtil.jsx
@@ -24,16 +24,22 @@ function getDiffTimeWithFormat(boardDate) {
   return boardDate.substring(2, 16).split('T')[0];
 }
 function getDiffTimeWithFormat2(boardDate) {
-  //시간까지 가져오기
+  //시간까지 가져오기(댓글에 사용)
   let now = new Date();
+  const utc = now.getTime() + now.getTimezoneOffset() * 60 * 1000; //현재 시간의 UTC 시간
+  const KR_TIME_DIFF = 9 * 60 * 60 * 1000; //UTC 시간과 KST 시간 차(밀리초)
+  const korNow = new Date(utc + KR_TIME_DIFF); //항상 현재 시간이 한국 시간 기준으로 측정되도록
   let board = new Date(boardDate.substring(0, 16).split('T'));
-  var sec = (now.getTime() - board.getTime()) / 1000;
+  var sec = (korNow.getTime() - board.getTime()) / 1000;
   if (sec < 60 * 60 * 24) {
+    if (sec < 60) {
+      return parseInt(sec) + '초 전';
+    }
     var min = parseInt(sec / 60);
-    var hour = parseInt(min / 60);
     if (min < 60) {
       return min + '분 전';
     }
+    var hour = parseInt(min / 60);
     return hour + '시간 전';
   }
   return getDateWithFormat(boardDate);
