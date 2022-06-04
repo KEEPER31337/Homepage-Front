276752830f455fedf452860748c9df6ecdee4b7c
publdaze <publdaze@naver.com>
2022-06-04 17:52:26 +0900

    feat) CTF API 작성
    - 출제자 지정 api url 수정
    - 문제 오픈 api 작성
    
    #146

--- src/API/v1/ctf.js
+++ src/API/v1/ctf.js
@@ -49,9 +49,28 @@ async function createProb({
 async function addAuthor({ memberId, token }) {
   const options = {
     method: 'POST',
-    url: API_URL + '/v1/admin/ctf/prob',
+    url: API_URL + '/v1/admin/ctf/prob/maker',
     data: {
       memberId,
+    },
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+async function openProb({ pid, token }) {
+  const options = {
+    method: 'PATCH',
+    url: API_URL + '/v1/admin/ctf/prob/' + pid + '/open',
+    data: {
+      pid,
     },
     headers: {
       Authorization: token,
@@ -68,4 +87,5 @@ async function addAuthor({ memberId, token }) {
 export default {
   createProb,
   addAuthor,
+  openProb,
 };
