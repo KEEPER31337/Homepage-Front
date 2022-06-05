9b7c24b56a45745abac01fd55980f3117ec43461
publdaze <publdaze@naver.com>
2022-06-05 16:32:04 +0900

    feat) ctf 문제 카테고리 api 작성
    - ctf 문제 카테고리 api 작성
    - ctf api (문제 오픈) 주석 추가
    
    #142

--- src/API/v1/ctf.js
+++ src/API/v1/ctf.js
@@ -79,6 +79,23 @@ async function getProbList({ token, cid }) {
   }
 }
 
+// CTF 문제 타입 조회
+async function getCategoryList({ token }) {
+  const options = {
+    method: 'GET',
+    url: API_URL + '/v1/ctf/extra/data/challenge-type',
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
 // CTF 문제 오픈
 async function openProb({ pid, token }) {
   const options = {
@@ -102,6 +119,7 @@ async function openProb({ pid, token }) {
 export default {
   createProb,
   addAuthor,
-  openProb,
   getProbList,
+  getCategoryList,
+  openProb,
 };
