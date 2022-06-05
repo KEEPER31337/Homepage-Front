123484e1b10744915819cf5fab42386905e0ae2d
publdaze <publdaze@naver.com>
2022-06-05 19:58:19 +0900

    feat) 대회 운영 관련 api 작성
    - CTF 대회 생성, CTF 개최, CTF 끝내기, CTF 대회 목록 보기 api 작성
    
    #146

--- src/API/v1/ctf.js
+++ src/API/v1/ctf.js
@@ -19,6 +19,83 @@ async function getRanking({ token, page, size, ctfId }) {
   }
 }
 
+// CTF 대회 생성
+async function createContest({ token, name, description }) {
+  const options = {
+    method: 'POST',
+    url: API_URL + '/v1/admin/ctf/contest',
+    data: {
+      name,
+      description,
+    },
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+// CTF 개최
+async function openContest({ cid, token }) {
+  const options = {
+    method: 'PATCH',
+    url: API_URL + '/v1/admin/ctf/contest/' + cid + '/open',
+    data: {
+      cid,
+    },
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+// CTF 끝내기
+async function closeContest({ cid, token }) {
+  const options = {
+    method: 'PATCH',
+    url: API_URL + '/v1/admin/ctf/contest/' + cid + '/close',
+    data: {
+      cid,
+    },
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+// CTF 대회 목록 보기
+async function getContestList({ token }) {
+  const options = {
+    method: 'GET',
+    url: API_URL + '/v1/admin/ctf/contests',
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
 
 // CTF 문제 생성
 async function createProb({
@@ -168,135 +245,139 @@ async function seeContests({ token }) {
 }
 
 async function createTeam({ name, description, contestId, token }) {
-    const options = {
-        method: 'POST',
-        url: API_URL + '/v1/ctf/team',
-        data: {
-        name,
-        description,
-        contestId,
-        },
-        headers: {
-        Authorization: token,
-        },
-    };
-    try {
-        const response = await axios(options);
-        return response.data;
-    } catch (error) {
-        return error.response.data;
-    }
+  const options = {
+    method: 'POST',
+    url: API_URL + '/v1/ctf/team',
+    data: {
+      name,
+      description,
+      contestId,
+    },
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 async function reviseTeam({ name, description, contestId, token }) {
-    const options = {
-        method: 'POST',
-        url: API_URL + '/v1/ctf/team/288',
-        data: {
-        name,
-        description,
-        contestId,
-        },
-        headers: {
-        Authorization: token,
-        },
-    };
-    try {
-        const response = await axios(options);
-        return response.data;
-    } catch (error) {
-        return error.response.data;
-    }
+  const options = {
+    method: 'POST',
+    url: API_URL + '/v1/ctf/team/288',
+    data: {
+      name,
+      description,
+      contestId,
+    },
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 async function joinTeam({ name, token }) {
-    const options = {
-        method: 'POST',
-        url: API_URL + '/v1/ctf/team',
-        data: {
-        name,
-        },
-        headers: {
-        Authorization: token,
-        },
-    };
-    try {
-        const response = await axios(options);
-        return response.data;
-    } catch (error) {
-        return error.response.data;
-    }
+  const options = {
+    method: 'POST',
+    url: API_URL + '/v1/ctf/team',
+    data: {
+      name,
+    },
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 async function resignTeam({ ctfId, token }) {
-    const options = {
-        method: 'DELETE',
-        url: API_URL + '/v1/ctf/team/member',
-        data: {
-            ctfId,
-        },
-        headers: {
-        Authorization: token,
-        },
-    };
-    try {
-        const response = await axios(options);
-        return response.data;
-    } catch (error) {
-        return error.response.data;
-    }
+  const options = {
+    method: 'DELETE',
+    url: API_URL + '/v1/ctf/team/member',
+    data: {
+      ctfId,
+    },
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 async function seeTeamList({ page, size, ctfId, token }) {
-    const options = {
-        method: 'GET',
-        url: API_URL + '/v1/ctf/team',
-        params: {
-            page:page,
-            size:size,
-            ctfId: ctfId,
-        },
-        headers: {
-        Authorization: token,
-        },
-    };
-    try {
-        const response = await axios(options);
-        return response.data;
-    } catch (error) {
-        return error.response.data;
-    }
+  const options = {
+    method: 'GET',
+    url: API_URL + '/v1/ctf/team',
+    params: {
+      page: page,
+      size: size,
+      ctfId: ctfId,
+    },
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 async function seeTeamDetail({ teamId, token }) {
-    const options = {
-        method: 'GET',
-        url: API_URL + '/v1/ctf/team/' + teamId,
-        headers: {
-        Authorization: token,
-        },
-    };
-    try {
-        const response = await axios(options);
-        return response.data;
-    } catch (error) {
-        return error.response.data;
-    }
+  const options = {
+    method: 'GET',
+    url: API_URL + '/v1/ctf/team/' + teamId,
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 export default {
-    createProb,
-    getRanking,
-    addAuthor,
-    getProbList,
-    getCategoryList,
-    getTypeList,
-    openProb,
-    seeContests,
-    createTeam,
-    reviseTeam,
-    joinTeam,
-    resignTeam,
-    seeTeamList,
-    seeTeamDetail,
+  createContest,
+  openContest,
+  closeContest,
+  getContestList,
+  createProb,
+  getRanking,
+  addAuthor,
+  getProbList,
+  getCategoryList,
+  getTypeList,
+  openProb,
+  seeContests,
+  createTeam,
+  reviseTeam,
+  joinTeam,
+  resignTeam,
+  seeTeamList,
+  seeTeamDetail,
 };
