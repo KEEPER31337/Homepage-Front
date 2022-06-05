d4b98e6b887af69cdc61cfe00834b925dbbd870f
pipisebastian <yrt7998@pusan.ac.kr>
2022-06-06 04:23:24 +0900

    feat) 테두리 제거
    - 테두리제거
    
    #140 #145 #146

--- src/page/CTF/admin/ChallengeAdmin.jsx
+++ src/page/CTF/admin/ChallengeAdmin.jsx
@@ -17,87 +17,65 @@ const ChallengeAdmin = ({ member, memberSignIn }) => {
         <NavigationLayout />
         <div className="md:w-4/5 flex flex-col flex-1 p-3">
           {/* 이제 여기서 추가할 컴포넌트 가져오면 됨!!! */}
-          <div className="">
-            <div className=" w-full container mx-auto justify-center items-center">
-              {/* 1. 커스텀 색상 팔레트 */}
 
-              <div className="md:flex p-1 border-2 shadow-sm">
-                {/* NOTE 프로필 */}
-                <div className="w-full m-2 ">
-                  <div className="p-1 bg-white">
-                    {/* 1.  닉네임 + 회원 뱃지 */}
-                    <div className="flex justify-between m-1">
-                      {/*닉네임*/}
-                      <div className="font-extrabold text-4xl m-1">
-                        Challenges-Admin
-                      </div>
-                      {/*회원 뱃지*/}
+          <div className="h-full w-full mx-auto justify-center items-center">
+            <div className="p-1 bg-white">
+              <div className="flex justify-between m-1">
+                <div className="font-extrabold text-4xl m-1">
+                  Challenges-Admin
+                </div>
 
-                      <div className="flex m-1">
-                        <div className="mr-1 flex rounded p-1 bg-amber-300 border-2 border-amber-400 shadow-[inset_0_2px_0_1px_#ffffff]">
-                          <div className=" text-md ">
-                            <button className="hover:bg-amber-500  m-1 hover:text-mainWhite rounded font-bold">
-                              <Link to="/ctf/admin/challengeWrite">
-                                문제추가
-                              </Link>
-                            </button>
-                          </div>
-                        </div>
-                        <div className="flex rounded p-1 bg-amber-300 border-2 border-amber-400 shadow-[inset_0_2px_0_1px_#ffffff]">
-                          <div className=" text-md ">
-                            <button className="hover:bg-amber-500  m-1 hover:text-mainWhite rounded font-bold">
-                              문제 삭제
-                            </button>
-                          </div>
-                        </div>
-                      </div>
+                <div className="flex m-1">
+                  <div className="mr-1 flex rounded p-1 bg-amber-300 border-2 border-amber-400 shadow-[inset_0_2px_0_1px_#ffffff]">
+                    <div className=" text-md ">
+                      <button className="hover:bg-amber-500  m-1 hover:text-mainWhite rounded font-bold">
+                        <Link to="/ctf/admin/challengeWrite">문제추가</Link>
+                      </button>
                     </div>
-                    {/*구분선*/}
-                    <div className="p-[2px] mb-2 bg-gradient-to-r from-amber-500 via-amber-200 to-yellow-300  "></div>
-                    {/* 2.  프로필 이미지 + 팔로우 + 포인트 */}
-
-                    <div className="w-full h-full inline-block rounded overflow-hidden text-center border">
-                      <table className="w-full border-2 shadow  rounded-md">
-                        <thead>
-                          <tr className="h-10 bg-gray-100  border-b-2">
-                            <th>ID</th>
-                            <th>title</th>
-                            <th>category</th>
-                            <th>score</th>
-                            <th>writer</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr className="w-full h-10 hover:bg-gray-100 dark:hover:bg-[#0b1523]">
-                            <td>1</td>
-                            <td>어려운 문제</td>
-                            <td>포렌식</td>
-                            <td>100</td>
-                            <td>김은지</td>
-                          </tr>
-                          <tr className="w-full h-10 hover:bg-gray-100 dark:hover:bg-[#0b1523]">
-                            <td>2</td>
-                            <td>너무 어려운 문제</td>
-                            <td>웹</td>
-                            <td>500</td>
-                            <td>김은지</td>
-                          </tr>
-                          <tr className="w-full h-10 hover:bg-gray-100 dark:hover:bg-[#0b1523]">
-                            <td>3</td>
-                            <td>짱짱 어려운 문제</td>
-                            <td>웹</td>
-                            <td>1000</td>
-                            <td>기믄지</td>
-                          </tr>
-                        </tbody>
-                      </table>
+                  </div>
+                  <div className="flex rounded p-1 bg-amber-300 border-2 border-amber-400 shadow-[inset_0_2px_0_1px_#ffffff]">
+                    <div className=" text-md ">
+                      <button className="hover:bg-amber-500  m-1 hover:text-mainWhite rounded font-bold">
+                        문제 삭제
+                      </button>
                     </div>
                   </div>
                 </div>
-                {/* NOTE 마이페이지 */}
-                {/* 1. 마이페이지(작성글) 컴포넌트*/}
               </div>
+              {/*구분선*/}
+              <div className="p-[2px] mb-2 bg-gradient-to-r from-amber-500 via-amber-200 to-yellow-300  "></div>
+
+              <table className="w-full h-full border-4 rounded  text-center">
+                <thead>
+                  <tr className="h-10 bg-gray-100  border-b-2">
+                    <th>ID</th>
+                    <th>title</th>
+                    <th>category</th>
+                    <th>score</th>
+                    <th>writer</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="w-full h-10 hover:bg-gray-100 dark:hover:bg-[#0b1523]">
+                    <td>1</td>
+                    <td>어려운 문제</td>
+                    <td>포렌식</td>
+                    <td>100</td>
+                    <td>김은지</td>
+                  </tr>
+                  <tr className="w-full h-10 hover:bg-gray-100 dark:hover:bg-[#0b1523]">
+                    <td>1</td>
+                    <td>어려운 문제</td>
+                    <td>포렌식</td>
+                    <td>100</td>
+                    <td>김은지</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
+
+            {/* NOTE 마이페이지 */}
+            {/* 1. 마이페이지(작성글) 컴포넌트*/}
           </div>
         </div>
       </div>
