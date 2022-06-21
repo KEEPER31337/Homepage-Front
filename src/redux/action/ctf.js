71d629ff7962c9f6653a8a6c84417454b71f2010
pipisebastian <yrt7998@pusan.ac.kr>
2022-06-21 22:34:36 +0900

    feat) actionCtf오타 제거 + 콘솔로그 제거

--- src/redux/action/ctf.js
+++ src/redux/action/ctf.js
@@ -1,9 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
 
-const actionMember = {
+const actionCtf = {
   updateId: createAction('ctf/updateId'),
   updateName: createAction('ctf/updateName'),
   updateTeamName: createAction('ctf/updateTeamName'),
 };
 
-export default actionMember;
+export default actionCtf;
