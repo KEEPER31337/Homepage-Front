e2bab3cb20b9c63d60924fa899d7b89d16e6f63e
pipisebastian <yrt7998@pusan.ac.kr>
2022-08-02 22:07:25 +0900

    feat) 투표하기 페이지 ui 수정
    - 맨처음엔 아무것도 선택 안되어있게
    - 선택한것들 뭔지 별표표시로 볼 수 있게
    - ui 수정
    
    #194

--- src/page/Vote/Components/VoteSelect.jsx
+++ src/page/Vote/Components/VoteSelect.jsx
@@ -1,11 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment, useState } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
-import {
-  CheckIcon,
-  SelectorIcon,
-  ChevronDownIcon,
-} from '@heroicons/react/solid';
+import { CheckIcon, StarIcon, ChevronDownIcon } from '@heroicons/react/solid';
 
 const people = [
   {
@@ -44,6 +40,18 @@ const people = [
     avatar:
       'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
+  {
+    id: 7,
+    name: '기믄지',
+    avatar:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 8,
+    name: '기믄지',
+    avatar:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
 ];
 
 function classNames(...classes) {
@@ -51,7 +59,11 @@ function classNames(...classes) {
 }
 
 export default function VoteSelect() {
-  const [selected, setSelected] = useState(people[0]);
+  const [selected, setSelected] = useState({
+    id: 0,
+    name: '',
+    avatar: '',
+  });
 
   return (
     <Listbox value={selected} onChange={setSelected}>
@@ -59,17 +71,22 @@ export default function VoteSelect() {
         <>
           <div className="mt-1 relative text-xl font-basic">
             <Listbox.Button className="relative rounded-md  w-full bg-white p-2">
-              <span className="flex items-center">
-                <img
-                  src={selected.avatar}
-                  alt=""
-                  className="flex-shrink-0 h-6 w-6 rounded-full"
-                />
+              <span className="flex items-center ">
+                {/* 맨처음, 아무것도 클릭하지 않았을때, avatar가 없을때 */}
+                {selected.avatar ? (
+                  <img
+                    src={selected.avatar}
+                    className="flex-shrink-0 h-7 w-7 rounded-full"
+                  />
+                ) : (
+                  ''
+                )}
+
                 <span className="ml-3 block truncate">{selected.name}</span>
               </span>
               <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                 <ChevronDownIcon
-                  className="h-7 w-7 text-gray-400"
+                  className="h-7 w-7 text-slate-400"
                   aria-hidden="true"
                 />
               </span>
@@ -78,17 +95,17 @@ export default function VoteSelect() {
             <Transition
               show={open}
               as={Fragment}
-              leave="transition ease-in duration-100"
+              leave="transition ease-in duration-150"
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 mt-1 rounded-md  w-full bg-white max-h-56  overflow-auto">
+              <Listbox.Options className="absolute z-10 mt-1 rounded-md shadow-sm border border-slate-100  w-full bg-white max-h-56  overflow-auto">
                 {people.map((person) => (
                   <Listbox.Option
                     key={person.id}
                     className={({ active }) =>
                       classNames(
-                        active ? ' bg-slate-100' : 'text-gray-900',
+                        active ? ' bg-slate-200' : 'text-gray-900',
                         'cursor-default select-none relative p-2'
                       )
                     }
@@ -111,6 +128,17 @@ export default function VoteSelect() {
                             {person.name}
                           </span>
                         </div>
+
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? 'text-white' : 'text-slate-500',
+                              'absolute inset-y-0 right-0 flex items-center pr-4'
+                            )}
+                          >
+                            <StarIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
                       </>
                     )}
                   </Listbox.Option>
--- src/page/Vote/MyPick.jsx
+++ src/page/Vote/MyPick.jsx
@@ -8,11 +8,13 @@ const MyPick = ({ member }) => {
   //투표여부 확인 api -> 했으면 -> 진행여부(실시간) 애니메이션만
   // 안했으면 ->
   return (
-    <div className="md:w-4/5 w-full p-3 text-xl font-basic">
+    <div className="md:w-4/5 w-full p-3 text-xl font-basic flex justify-center">
       <div className="h-full w-full flex container justify-center bg-slate-50 items-center p-3">
-        <div className="h-full md:w-5/12 w-full flex flex-col p-3 bg-white ">
-          <div className="mt-2 mb-4 text-center"> 2022년 2학기 </div>
-          <div className="flex flex-row w-full ">
+        {/* 투표용지 */}
+        <div className="h-full md:w-5/12 w-full flex flex-col p-3 bg-white border border-slate-100 shadow-md">
+          <div className="mt-2 mb-4 text-center "> 2022년 2학기 </div>
+
+          <div className="flex flex-row w-full">
             <div className="w-2/6 items-center p-2">회장</div>
             <div className="w-4/6">
               <VoteSelect />
@@ -33,6 +35,13 @@ const MyPick = ({ member }) => {
             <div className="w-4/6">
               <VoteSelect />
             </div>
+          </div>
+          <div className="flex justify-between">
+            <div></div>
+
+            <button class="bg-slate-100 border-slate-300 rounded border-b-4 mt-4 px-4 py-1  hover:bg-slate-200">
+              투표
+            </button>
           </div>
         </div>
       </div>
