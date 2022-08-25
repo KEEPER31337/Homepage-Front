ff1f394f4c20cd896fc9c3c3298d00b05a156134
amaran-th <songsy405@gmail.com>
2022-08-26 05:36:10 +0900

    feat: 상벌점 누계 페이지 구현
    - 상벌점 누계 페이지 회원별 상벌점 목록&랭킹 UI 구현
    
    #213

--- src/page/Clerk/Components/ReasonOfPoint/PointUtil.jsx
+++ src/page/Clerk/Components/ReasonOfPoint/PointUtil.jsx
@@ -1,8 +1,26 @@
 //여러 파일에서 사용할 함수 모음
-
+const compareName = (a, b) => {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+};
+const compareRanking = (a, b) => {
+  if (a.plusP - a.minusP < b.plusP - b.minusP) {
+    return 1;
+  } else if (a.plusP - a.minusP > b.plusP - b.minusP) {
+    return -1;
+  }
+  if (a.plusP < b.plusP) return 1;
+  else if (a.plusP > b.plusP) return -1;
+  return 0;
+};
 function getNow() {
   //오늘 날짜 반환
   return new Date().toISOString().substring(0, 10);
 }
 
-export { getNow };
+export { compareName, compareRanking, getNow };
