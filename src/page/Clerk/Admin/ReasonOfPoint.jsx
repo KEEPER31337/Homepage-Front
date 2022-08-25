4ecc52b3a50c560328e21a1436b2a0c687b67a24
amaran-th <songsy405@gmail.com>
2022-08-26 01:19:20 +0900

    feat : 상벌점 사유 페이지 상벌점 내역 조회 UI 구현
    #214

--- src/page/Clerk/Admin/ReasonOfPoint.jsx
+++ src/page/Clerk/Admin/ReasonOfPoint.jsx
@@ -3,11 +3,60 @@ import AuthUser from 'shared/AuthUser';
 import { PlusCircleIcon } from '@heroicons/react/solid';
 
 import WriteTable from '../Components/ReasonOfPoint/WriteTable';
+import ViewTable from '../Components/ReasonOfPoint/ViewTable';
+const years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
+const testData = [
+  {
+    date: '2022-08-16',
+    name: '김김김',
+    reason: '무단결석',
+    plusP: 0,
+    minusP: 2,
+  },
+  {
+    date: '2022-08-16',
+    name: '이이이',
+    reason: '회비 미납부',
+    plusP: 0,
+    minusP: 1,
+  },
+  {
+    date: '2022-08-16',
+    name: '아무개',
+    reason: '각종 대외발표',
+    plusP: 2,
+    minusP: 0,
+  },
+  {
+    date: '2022-08-22',
+    name: '정정정',
+    reason: '무단결석',
+    plusP: 0,
+    minusP: 2,
+  },
+  {
+    date: '2022-08-22',
+    name: '박박박',
+    reason: '지각 2회',
+    plusP: 0,
+    minusP: 2,
+  },
+  {
+    date: '2022-08-22',
+    name: '김김김',
+    reason: '그냥',
+    plusP: 1,
+    minusP: 0,
+  },
+];
 
 const ReasonOfPoint = () => {
   const [appendData, setAppendData] = useState([]);
+  const [recordData, setRecordData] = useState([]);
+  const [curYear, setCurYear] = useState();
 
   const getNow = () => {
+    //오늘 날짜 반환
     return new Date().toISOString().substring(0, 10);
   };
   const addData = () => {
@@ -24,6 +73,7 @@ const ReasonOfPoint = () => {
       },
     ]);
   };
+
   useEffect(() => {
     setAppendData([
       {
@@ -36,11 +86,13 @@ const ReasonOfPoint = () => {
         date: getNow(),
       },
     ]);
+    setRecordData(testData);
   }, []);
 
   useEffect(() => {
     console.log(appendData);
   }, [appendData]);
+
   return (
     <AuthUser>
       <div className="flex flex-1 justify-center">
@@ -77,8 +129,32 @@ const ReasonOfPoint = () => {
             className="flex flex-col gap-y-2 rounded-md border w-full p-2 bg-gray-100"
           >
             <p className="text-2xl">상벌점 내역</p>
+            <div name="연도 선택"></div>
             <div className="flex flex-col gap-y-2 pl-2">
-              ~연도별로 분류해서 볼 수 있게 만들 예정~
+              <div className="flex flex-wrap gap-2 justify-center p-4">
+                {years.map((year, index) => (
+                  <button
+                    key={index}
+                    className={
+                      (curYear === year
+                        ? 'ring-2 ring-violet-400 ring-offset-4 ring-offset-gray-100 bg-violet-400'
+                        : 'bg-violet-200 hover:bg-violet-400') +
+                      ' shadow-sm rounded-md p-1 px-4 focus:outline-none'
+                    }
+                    onClick={() => {
+                      setCurYear(year);
+                    }}
+                    disabled={curYear === year}
+                  >
+                    {year}
+                  </button>
+                ))}
+              </div>
+              <ViewTable
+                curYear={curYear}
+                recordData={recordData}
+                setRecordData={setRecordData}
+              />
             </div>
           </div>
         </div>
--- src/page/Clerk/Components/ReasonOfPoint/ViewTable.jsx
+++ src/page/Clerk/Components/ReasonOfPoint/ViewTable.jsx
@@ -1,30 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
-const ViewTable = ({ pointBreakdown, setBreakdown }) => {
+const ViewTable = ({ curYear, recordData, setRecordData }) => {
   useEffect(() => {}, []);
+  useEffect(() => {
+    console.log(curYear);
+    setRecordData(recordData);
+  }, [curYear]);
 
   return (
-    <table className="w-full">
-      <tr className="flex border-b text-left">
-        <th className="min-w-[2em] w-[2em] px-1">No</th>
-        <div className="flex w-full">
-          <th className="min-w-[7em] px-1">날짜</th>
-          <th className="min-w-[7em] w-full sm:w-[10em] px-1">이름</th>
-          <th className="min-w-[6em] w-[6em] sm:w-[10em] px-1">상/벌점</th>
-          <th className="hidden sm:table-cell w-full px-1">사유</th>
-          <th className="hidden sm:table-cell min-w-[6em] w-[6em] px-1">
-            점수
-          </th>
-        </div>
-      </tr>
-      {pointData.map((data) => (
-        <WriteTableCell
-          no={data.no}
-          pointData={pointData}
-          setPointData={setPointData}
-        />
-      ))}
-    </table>
+    <div className="bg-mainWhite rounded-md p-2">
+      <table className="w-full text-center">
+        <tr className="flex border-b-2 w-full">
+          <th className="min-w-[2em] w-[2em] px-1">No</th>
+          <div className="flex w-full">
+            <th className="min-w-[7em] w-[7em] px-1">날짜</th>
+            <th className="min-w-[4em] w-full px-1">이름</th>
+            {/**모바일에서 내역을 클릭하면 모달창으로 사유를 띄워주도록*/}
+            <th className="hidden sm:block sm:w-full px-1">사유</th>
+            <th className="min-w-[3em] sm:w-[6em] px-1">상점</th>
+            <th className="min-w-[3em] sm:w-[6em] px-1">벌점</th>
+          </div>
+        </tr>
+        {recordData.map((data, index) => (
+          <tr key={index} className="flex border-b">
+            <td className="min-w-[2em] w-[2em] p-1">{index + 1}</td>
+            <div className="flex w-full">
+              <td className="min-w-[7em] w-[7em] p-1">{data.date}</td>
+              <td className="min-w-[4em] w-full p-1">{data.name}</td>
+              {/**모바일에서 내역을 클릭하면 모달창으로 사유를 띄워주도록*/}
+              <td className="hidden sm:block sm:w-full p-1 text-left">
+                {data.reason}
+              </td>
+              <td className="min-w-[3em] sm:w-[6em] p-1 border-x">
+                {data.plusP !== 0 ? data.plusP : ''}
+              </td>
+              <td className="min-w-[3em] sm:w-[6em] p-1">
+                {data.minusP !== 0 ? data.minusP : ''}
+              </td>
+            </div>
+          </tr>
+        ))}
+      </table>
+    </div>
   );
 };
 
