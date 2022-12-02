8a42e696e014888ac82fbb5a73c26730e5a8bb50
lcqff <gooyeonhu82@gmail.com>
2022-12-02 16:51:19 +0900

    fix : 세미나 출석 관련 알람 추가 - 세미나 출석 완료시 출석 완료 페이지 띄움 - 세미나가 생성되지 않은 날짜에 출석 시작 누를 시 '생성된 세미나 없음' 알람 띄우기 - 출석 시간이 지나고 코드 입력시 '출석 시간이 지난 세미나' 알람 띄우기 #259

--- src/page/Clerk/Components/AutoAttend/DoAttend.jsx
+++ src/page/Clerk/Components/AutoAttend/DoAttend.jsx
@@ -1,115 +1,132 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 import { connect } from 'react-redux';
-import { useLocation } from "react-router-dom"
-import AuthUser from "shared/AuthUser";
+import { useLocation } from 'react-router-dom';
+import AuthUser from 'shared/AuthUser';
 import ReactCodeInput from 'react-code-input';
 
 //local
 import attendAPI from 'API/v1/clerk';
+import attendance from 'API/v1/attendance';
 
 //출석 시작 버튼을 눌렀을 때 뜨는 페이지
-const DoAttend = ({member, state}) => {
-    const props = {
-        inputStyle: {
-          fontFamily: 'monospace',
-          margin:  '4px',
-          MozAppearance: 'textfield',
-          width: '35px',
-          borderRadius: '3px',
-          fontSize: '20px',
-          height: '35px',
-          paddingLeft: '7px',
-          backgroundColor: 'white',
-          color: '#d6bcfa',
-          border: '1px solid #d6bcfa',
-          textAlign: 'center'
-        },
-        inputStyleInvalid: {
-          fontFamily: 'monospace',
-          margin:  '4px',
-          MozAppearance: 'textfield',
-          width: '15px',
-          borderRadius: '3px',
-          fontSize: '14px',
-          height: '26px',
-          paddingLeft: '7px',
-          backgroundColor: 'white',
-          color: 'red',   
-          border: '2px solid red'
+const DoAttend = ({ member, state }) => {
+  const props = {
+    inputStyle: {
+      fontFamily: 'monospace',
+      margin: '4px',
+      MozAppearance: 'textfield',
+      width: '35px',
+      borderRadius: '3px',
+      fontSize: '20px',
+      height: '35px',
+      paddingLeft: '7px',
+      backgroundColor: 'white',
+      color: '#d6bcfa',
+      border: '1px solid #d6bcfa',
+      textAlign: 'center',
+    },
+    inputStyleInvalid: {
+      fontFamily: 'monospace',
+      margin: '4px',
+      MozAppearance: 'textfield',
+      width: '15px',
+      borderRadius: '3px',
+      fontSize: '14px',
+      height: '26px',
+      paddingLeft: '7px',
+      backgroundColor: 'white',
+      color: 'red',
+      border: '2px solid red',
+    },
+  };
+  const location = useLocation(); //location.state로 인자값 받아옴
+  const token = state.member.token;
+  const [today, setToday] = useState(location.state.currentTime.toString()); //바로 currnetT 스테이트 가져옴
+  const [inputCode, setInputCode] = useState('');
+  const [seminarId, setSeminarId] = useState();
+  cosnt[(attendance, setAttendance)] = useState(false);
+  useEffect(() => {
+    attendAPI
+      .getSeminar({
+        searchDate: today.substr(0, 10),
+        token: token,
+      })
+      .then((res) => {
+        //console.log(res);
+        if (res.success) {
+          //console.log(res.msg);
+          setSeminarId(res.data.seminarId);
+        } else alert('목록을 불러오는데 실패했습니다.');
+      });
+  }, []);
+
+  const CodeInput = (event) => {
+    //console.log(event);
+    setInputCode(event);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    //console.log(inputCode, seminarId);
+    attendAPI
+      .AttendSeminar({
+        token: token,
+        seminarId: seminarId,
+        attendanceCode: inputCode,
+      })
+      .then((res) => {
+        if (res.success) {
+          if (res.data.isPossibleAttendance) {
+            alert(res.data.attendanceStatus);
+            setAttendance(true);
+          } else alert('세미나 출석이 종료되었습니다.');
         }
-      }
-    const location = useLocation(); //location.state로 인자값 받아옴
-    const token = state.member.token;
-    const [today,setToday] = useState(location.state.currentTime.toString()); //바로 currnetT 스테이트 가져옴
-    const [inputCode, setInputCode] = useState('');
-    const [seminarId,setSeminarId] = useState();
-    useEffect(() => {
-        attendAPI
-            .getSeminar({
-                searchDate: today.substr(0,10),
-                token: token,
-            })
-            .then((res) => { 
-                //console.log(res);
-                if(res.success) {
-                    //console.log(res.msg);
-                    setSeminarId(res.data.seminarId);
-                }
-                else alert("목록을 불러오는데 실패했습니다.");
-            });
-    },[])
+      });
+  };
 
-    const CodeInput = (event) => {
-        //console.log(event);
-        setInputCode(event);
-      }
-
-    const onSubmit = (event) => {
-        event.preventDefault();
-        //console.log(inputCode, seminarId);
-        attendAPI.
-          AttendSeminar({
-            token: token,
-            seminarId: seminarId,
-            attendanceCode: inputCode,
-          })
-          .then((res) => {
-            if (res.success) {
-                //alert (res.msg);
-                alert(res.data.attendanceStatus);}
-            else alert("세미나 출석 실패");
-          })
-      }
-
-    return (
+  return (
     <AuthUser>
-        <div className="min-h-screen dark:bg-black">
+      <div className="min-h-screen dark:bg-black">
         <div className="container mx-auto p-4 bg-white dark:bg-black">
-            <div className="flex flex-1 justify-center w-full ">
-              <div className="w-3/4 mt-32 font-bold text-center text-violet-300 space-y-8">
-                <label className="py-2 px-6 bg-violet-300 text-white text-lg rounded-3xl dark:text-black">코드를 입력하세요</label><br/>
-                <form className="flex flex-col mt-4">
-                  <ReactCodeInput onChange={CodeInput} value="" type="text" fields={4} {...props} />
-                  <button
+          <div className="flex flex-1 justify-center w-full ">
+            <div className="w-3/4 mt-32 font-bold text-center text-violet-300 space-y-8">
+              {attendance ? (
+                <div> 출석완료 </div>
+              ) : (
+                <>
+                  <label className="py-2 px-6 bg-violet-300 text-white text-lg rounded-3xl dark:text-black">
+                    코드를 입력하세요
+                  </label>
+                  <br />
+                  <form className="flex flex-col mt-4">
+                    <ReactCodeInput
+                      onChange={CodeInput}
+                      value=""
+                      type="text"
+                      fields={4}
+                      {...props}
+                    />
+                    <button
                       onClick={onSubmit}
                       className="w-1/4 mx-auto mt-8 px-4 py-2 bg-violet-200 leading-6 rounded-md border border-transparent text-white focus:outline-none 
-                      hover:bg-violet-300 items-center w-full justify-center items-center font-bold focus:outline-none dark:text-black"
-                  >
-                    출석
-                  </button>    
-                </form>
-              </div>
+                          hover:bg-violet-300 items-center w-full justify-center items-center font-bold focus:outline-none dark:text-black"
+                    >
+                      출석
+                    </button>
+                  </form>
+                </>
+              )}
             </div>
           </div>
-          </div>
+        </div>
+      </div>
     </AuthUser>
-    );
+  );
 };
 
-
 const mapStateToProps = (state) => {
-  return { member: state.member, state:state };
+  return { member: state.member, state: state };
 };
 
 const mapDispatchToProps = (dispatch, OwnProps) => {
