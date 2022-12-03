c0257606676ba39bfc7826b722f035b84e9e8252
amaran-th <songsy405@gmail.com>
2022-12-03 13:10:13 +0900

    refactor : compareRanking 함수 리팩토링
    #311

--- src/page/Clerk/Components/ReasonOfPoint/PointUtil.jsx
+++ src/page/Clerk/Components/ReasonOfPoint/PointUtil.jsx
@@ -9,14 +9,10 @@ const compareName = (a, b) => {
   return 0;
 };
 const compareRanking = (a, b) => {
-  if (a.totalMerit < b.totalMerit) {
-    return 1;
-  } else if (a.totalMerit > b.totalMerit) {
-    return -1;
+  if (a.totalMerit === b.totalMerit) {
+    return a.totalDemerit > b.totalDemerit;
   }
-  if (a.totalDemerit > b.totalDemerit) return 1;
-  else if (a.totalDemerit < b.totalDemerit) return -1;
-  return 0;
+  return a.totalMerit < b.totalMerit;
 };
 function getNow() {
   //오늘 날짜 반환
