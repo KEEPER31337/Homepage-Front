9dbbab32168c0d04072e561fc7e6d54a73adf969
publdaze <publdaze@naver.com>
2022-12-08 18:07:43 +0900

    feat : 문제 제출 시 마다 문제 정보 업데이트 해주기
    - flagCheckMsg에서 중복되는 부분과 변수명과 더 어울리는 데이터로 string 값만 넣도록 해주었습니다
    - 제출할 때마다 문제 정보를 업데이트 해줘야 하므로 flagCheckMsg가 변경될 때마다 api를 호출하도록 해주었습니다.
    - 문제 정보가 상위 컴포넌트에서 props로 받아오고 있었는데 상위 컴포넌트에서는 사용되지 않아 직접적으로 사용하는 부분에서 바로 api를 호출하도록 변경해주었습니다.
    - #309

--- src/page/CTF/Components/ChallengeModal.jsx
+++ src/page/CTF/Components/ChallengeModal.jsx
@@ -18,8 +18,23 @@ import ctfAPI from 'API/v1/ctf';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-const ChallengeModal = forwardRef(({ detailProbList, member }, ref) => {
+const ChallengeModal = forwardRef(({ pid, member }, ref) => {
   const [open, setOpen] = useState(false);
+  const [detailProbList, setDetailProbList] = useState({
+    challengeId: null,
+    title: null,
+    content: null,
+    category: {
+      id: null,
+      name: null,
+    },
+    score: null,
+    creatorName: null,
+    contestId: null,
+    solvedTeamCount: null,
+    isSolved: null,
+    file: null,
+  });
 
   useImperativeHandle(ref, () => ({
     open: () => {
@@ -36,62 +51,83 @@ const ChallengeModal = forwardRef(({ detailProbList, member }, ref) => {
   }, [detailProbList.content]); */
 
   const [flag, setFlag] = useState('');
-  const [flagCheckMsg, setFlagCheckMsg] = useState(<></>);
+  const [flagCheckMsg, setFlagCheckMsg] = useState(null);
   const onChange = (e) => {
     setFlag(e.target.value);
   };
 
+  const getMsgColor = () => {
+    let color;
+    switch (flagCheckMsg) {
+      case '정답':
+        color = 'green';
+        break;
+      case '오답':
+        color = 'red';
+        break;
+      default:
+        color = 'gray';
+    }
+
+    return `border-${color}-300 bg-${color}-200 text-${color}-800`;
+  };
+
   const onClick = () => {
+    if (flagCheckMsg === '정답') window.location.reload();
+
     setOpen(false);
-    if (flagCheckMsg.props.children === '정답') window.location.reload();
+    setFlagCheckMsg(null);
   };
 
   const submitFlagHandler = () => {
-    if (!detailProbList.remainedSubmitCount) {
-      setFlagCheckMsg(
-        <div className="border-2 border-gray-300 bg-gray-200 w-full p-1 rounded-md text-center text-gray-800">
-          남은 제출 횟수가 없습니다.
-        </div>
-      );
+    const { remainedSubmitCount, lastTryTime, isSolved, challengeId } =
+      detailProbList;
+
+    if (isSolved) {
+      setFlagCheckMsg('이미 해치운 문제입니다.');
       return;
     }
 
-    if (detailProbList.isSolved) {
-      setFlagCheckMsg(
-        <div className="border-2 border-gray-300 bg-gray-200 w-full p-1 rounded-md text-center text-gray-800">
-          이미 해치운 문제입니다.
-        </div>
-      );
-    } else {
-      ctfAPI
-        .submitFlag({
-          pid: detailProbList.challengeId,
-          content: flag,
-          token: member.token,
-        })
-        .then((data) => {
-          if (data.success) {
-            // console.log(data);
-            if (data.data.isCorrect) {
-              setFlagCheckMsg(
-                <div className="border-2 border-green-300 bg-green-200 w-full p-1 rounded-md text-center text-green-800">
-                  정답
-                </div>
-              );
-            } else {
-              setFlagCheckMsg(
-                <div className="border-2 border-red-300 bg-red-200 w-full p-1 rounded-md text-center text-red-800">
-                  오답
-                </div>
-              );
-            }
-          } else {
-            // console.log(data);
-            alert('flag 제출중 오류가 발생하였습니다.');
-          }
-        });
+    if (!remainedSubmitCount) {
+      setFlagCheckMsg('남은 제출 횟수가 없습니다.');
+      return;
     }
+
+    ctfAPI
+      .submitFlag({
+        pid: challengeId,
+        content: flag,
+        token: member.token,
+      })
+      .then((data) => {
+        if (data.success) {
+          if (data.data.isCorrect) {
+            setFlagCheckMsg('정답');
+          } else {
+            setFlagCheckMsg('오답');
+          }
+        } else {
+          alert('flag 제출중 오류가 발생하였습니다.');
+        }
+      });
   };
+
+  useEffect(() => {
+    if (!pid) return;
+
+    ctfAPI
+      .getDetailProbList({
+        pid,
+        token: member.token,
+      })
+      .then((data) => {
+        if (data.success) {
+          setDetailProbList(data.data);
+        } else {
+          alert('문제 세부 목록을 받아오는 중 오류가 발생하였습니다.');
+        }
+      });
+  }, [flagCheckMsg]);
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -193,7 +229,13 @@ const ChallengeModal = forwardRef(({ detailProbList, member }, ref) => {
                           {`(${detailProbList.remainedSubmitCount} / ${detailProbList.maxSubmitCount})`}
                         </button>
                       </div>
-                      {flagCheckMsg}
+                      {flagCheckMsg && (
+                        <div
+                          className={`border-2 w-full p-1 rounded-md text-center ${getMsgColor()}`}
+                        >
+                          {flagCheckMsg}
+                        </div>
+                      )}
                     </div>
                   </div>
                 </div>
